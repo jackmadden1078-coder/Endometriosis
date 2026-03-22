2e5d9aa6ee3d2aed6958c2de614b5ee70c53d8e1
jackmadden1078-coder <jackmadden1078@gmail.com>
2026-03-21 21:10:10 -0400

    Add endometriosis companion app component

---
+++ endometriosis-app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EndometriosisApp = () => {
+    return (
+        <div>
+            <h1>Endometriosis Companion App</h1>
+            <p>This app provides information and resources for those affected by endometriosis.</p>
+        </div>
+    );
+};
+
+export default EndometriosisApp;
